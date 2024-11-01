47f51dba0075cf9f2aeba9a3b25afa8dd249be9e
moharanger <m33537924@gmail.com>
2024-11-01 14:17:10 +0330

    ᴀᴘʟʜᴀ

--- SONALI/__main__.py
+++ SONALI/__main__.py
@@ -37,6 +37,7 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("SONALI.plugins" + all_module)
+        
     if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module    
