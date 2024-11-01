a1ce09b89c917bba459e27f3886cae8fa8d80d49
moharanger <m33537924@gmail.com>
2024-11-01 16:31:08 +0330

    Update start.py

--- SONALI/plugins/bot/start.py
+++ SONALI/plugins/bot/start.py
@@ -150,6 +150,8 @@ async def start_gp(client, message: Message, _):
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
+    
+    
 
 
 @app.on_message(filters.new_chat_members, group=-1)
@@ -193,3 +195,296 @@ async def welcome(client, message: Message):
                 await message.stop_propagation()
         except Exception as ex:
             print(ex)
+            
+@app.on_message(filters.command(["start","شروع"] , prefixes=["", "/"]) & filters.private & ~BANNED_USERS)
+@LanguageStart
+async def start_comm(client, message: Message, _):
+    chat_id = message.chat.id
+    await add_served_user(message.from_user.id)
+    if len(message.text.split()) > 1:
+        name = message.text.split(None, 1)[1]
+        if name[0:4] == "help":
+            keyboard = InlineKeyboardMarkup(
+                paginate_modules(0, HELPABLE, "help", close=True)
+            )
+            if config.START_IMG_URL:
+                return await message.reply_photo(
+                    photo=START_IMG_URL,
+                    caption=_["help_1"],
+                    reply_markup=keyboard,
+                )
+            else:
+                return await message.reply_text(
+                    text=_["help_1"],
+                    reply_markup=keyboard,
+                )
+        if name[0:4] == "song":
+            await message.reply_text(_["song_2"])
+            return
+        if name == "mkdwn_help":
+            await message.reply(
+                MARKDOWN,
+                parse_mode=ParseMode.HTML,
+                disable_web_page_preview=True,
+            )
+        if name == "greetings":
+            await message.reply(
+                WELCOMEHELP,
+                parse_mode=ParseMode.HTML,
+                disable_web_page_preview=True,
+            )
+        if name[0:3] == "sta":
+            m = await message.reply_text("🔎 گرفتن مشخصات شخصی شما.!")
+            stats = await get_userss(message.from_user.id)
+            tot = len(stats)
+            if not stats:
+                await asyncio.sleep(1)
+                return await m.edit(_["ustats_1"])
+
+            def get_stats():
+                msg = ""
+                limit = 0
+                results = {}
+                for i in stats:
+                    top_list = stats[i]["spot"]
+                    results[str(i)] = top_list
+                    list_arranged = dict(
+                        sorted(
+                            results.items(),
+                            key=lambda item: item[1],
+                            reverse=True,
+                        )
+                    )
+                if not results:
+                    return m.edit(_["ustats_1"])
+                tota = 0
+                videoid = None
+                for vidid, count in list_arranged.items():
+                    tota += count
+                    if limit == 10:
+                        continue
+                    if limit == 0:
+                        videoid = vidid
+                    limit += 1
+                    details = stats.get(vidid)
+                    title = (details["title"][:35]).title()
+                    if vidid == "telegram":
+                        msg += f"🔗[ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇs ᴀɴᴅ ᴀᴜᴅɪᴏs]({config.SUPPORT_GROUP}) ** played {count} ᴛɪᴍᴇs**\n\n"
+                    else:
+                        msg += f"🔗 [{title}](https://www.youtube.com/watch?v={vidid}) ** played {count} times**\n\n"
+                msg = _["ustats_2"].format(tot, tota, limit) + msg
+                return videoid, msg
+
+            try:
+                videoid, msg = await loop.run_in_executor(None, get_stats)
+            except Exception as e:
+                print(e)
+                return
+            thumbnail = await YouTube.thumbnail(videoid, True)
+            await m.delete()
+            await message.reply_photo(photo=thumbnail, caption=msg)
+            return
+        if name[0:3] == "sud":
+            await sudoers_list(client=client, message=message, _=_)
+            await asyncio.sleep(1)
+            if await is_on_off(config.LOG):
+                sender_id = message.from_user.id
+                sender_mention = message.from_user.mention
+                sender_name = message.from_user.first_name
+                return await app.send_message(
+                    config.LOG_GROUP_ID,
+                    f"{message.from_user.mention} فقط ربات را برای تست اجرا کرده است<code> مشخصات ویدیو </code>\n\n**ایدی کاربر:** {sender_id}\n**نام کاربر** {sender_name}",
+                )
+            return
+        if name[0:3] == "lyr":
+            query = (str(name)).replace("lyrics_", "", 1)
+            lyrical = config.lyrical
+            lyrics = lyrical.get(query)
+            if lyrics:
+                await Telegram.send_split_text(message, lyrics)
+                return
+            else:
+                await message.reply_text("شکست خورد.")
+                return
+        if name[0:3] == "del":
+            await del_plist_msg(client=client, message=message, _=_)
+            await asyncio.sleep(1)
+        if name[0:3] == "inf":
+            m = await message.reply_text("🔎 گرفتن مشخصات!")
+            query = (str(name)).replace("info_", "", 1)
+            query = f"https://www.youtube.com/watch?v={query}"
+            results = VideosSearch(query, limit=1)
+            for result in (await results.next())["result"]:
+                title = result["title"]
+                duration = result["duration"]
+                views = result["viewCount"]["short"]
+                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                channellink = result["channel"]["link"]
+                channel = result["channel"]["name"]
+                link = result["link"]
+                published = result["publishedTime"]
+            searched_text = f"""
+🔍__**مشخصات ویدیو**__
+
+❇️**عنوان:** {title}
+
+⏳**مدت:** {duration} دقیقه
+👀**تماشا شده:** `{views}`
+⏰**زمان انتشار:** {published}
+🎥**نام کانال:** {channel}
+📎**لینک کانال:** [از اینجا تماشا کنید]({channellink})
+🔗**لینک ویدیو:** [لینک]({link})
+"""
+            key = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="🎥 ᴡᴀᴛᴄʜ ", url=f"{link}"),
+                        InlineKeyboardButton(text="🔄 ᴄʟᴏsᴇ", callback_data="close"),
+                    ],
+                ]
+            )
+            await m.delete()
+            await app.send_photo(
+                message.chat.id,
+                photo=thumbnail,
+                caption=searched_text,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_markup=key,
+            )
+            await asyncio.sleep(1)
+            if await is_on_off(config.LOG):
+                sender_id = message.from_user.id
+                sender_name = message.from_user.first_name
+                return await app.send_message(
+                    config.LOG_GROUP_ID,
+                    f"{message.from_user.mention} فقط ربات را برای تست اجرا کرده است<code> مشخصات ویدیو </code>\n\n**ایدی کاربر:** {sender_id}\n**نام کاربر** {sender_name}",
+                )
+    else:
+        try:
+            await app.resolve_peer(OWNER_ID[0])
+            OWNER = OWNER_ID[0]
+        except:
+            OWNER = None
+        out = private_panel(_, app.username, OWNER)
+        if config.START_IMG_URL:
+            try:
+                await message.reply_photo(
+                    photo=config.START_IMG_URL,
+                    caption=_["start_1"].format(app.mention),
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
+            except:
+                await message.reply_text(
+                    text=_["start_1"].format(app.mention),
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
+        else:
+            await message.reply_text(
+                text=_["start_1"].format(app.mention),
+                reply_markup=InlineKeyboardMarkup(out),
+            )
+        if await is_on_off(config.LOG):
+            sender_id = message.from_user.id
+            sender_name = message.from_user.first_name
+            return await app.send_message(
+                config.LOG_GROUP_ID,
+                f"{message.from_user.mention} ʜᴀs sᴛᴀʀᴛᴇᴅ ʙᴏᴛ. \n\n**ᴜsᴇʀ ɪᴅ :** {sender_id}\n**ᴜsᴇʀ ɴᴀᴍᴇ:** {sender_name}",
+            )
+
+
+@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
+@LanguageStart
+async def testbot(client, message: Message, _):
+    out = alive_panel(_)
+    uptime = int(time.time() - _boot_)
+    chat_id = message.chat.id
+    if config.START_IMG_URL:
+        await message.reply_photo(
+            photo=config.START_IMG_URL,
+            caption=_["start_7"].format(app.mention, get_readable_time(uptime)),
+            reply_markup=InlineKeyboardMarkup(out),
+        )
+    else:
+        await message.reply_text(
+            text=_["start_7"].format(app.mention, get_readable_time(uptime)),
+            reply_markup=InlineKeyboardMarkup(out),
+        )
+    return await add_served_chat(message.chat.id)
+
+
+@app.on_message(filters.new_chat_members, group=-1)
+async def welcome(client, message: Message):
+    chat_id = message.chat.id
+    if config.PRIVATE_BOT_MODE == str(True):
+        if not await is_served_private_chat(message.chat.id):
+            await message.reply_text(
+                "**ᴛʜɪs ʙᴏᴛ's ᴘʀɪᴠᴀᴛᴇ ᴍᴏᴅᴇ ʜᴀs ʙᴇᴇɴ ᴇɴᴀʙʟᴇᴅ ᴏɴʟʏ ᴍʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ɪғ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴛʜɪs ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ sᴏ sᴀʏ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ ᴛᴏ ᴀᴜᴛʜᴏʀɪᴢᴇ ʏᴏᴜʀ ᴄʜᴀᴛ."
+            )
+            return await app.leave_chat(message.chat.id)
+    else:
+        await add_served_chat(chat_id)
+    for member in message.new_chat_members:
+        try:
+            language = await get_lang(message.chat.id)
+            _ = get_string(language)
+            if member.id == app.id:
+                chat_type = message.chat.type
+                if chat_type != ChatType.SUPERGROUP:
+                    await message.reply_text(_["start_5"])
+                    return await app.leave_chat(message.chat.id)
+                if chat_id in await blacklisted_chats():
+                    await message.reply_text(
+                        _["start_6"].format(
+                            f"https://t.me/{app.username}?start=sudolist"
+                        )
+                    )
+                    return await app.leave_chat(chat_id)
+                userbot = await get_assistant(message.chat.id)
+                out = start_pannel(_)
+                await message.reply_text(
+                    _["start_2"].format(
+                        app.mention,
+                        userbot.username,
+                        userbot.id,
+                    ),
+                    reply_markup=InlineKeyboardMarkup(out),
+                )
+            if member.id in config.OWNER_ID:
+                return await message.reply_text(
+                    _["start_3"].format(app.mention, member.mention)
+                )
+            if member.id in SUDOERS:
+                return await message.reply_text(
+                    _["start_4"].format(app.mention, member.mention)
+                )
+            return
+        except:
+
+            return
+
+
+__MODULE__ = "ربات"
+__HELP__ = f"""
+<b>✦ c به معنای پخش کانال است.</b>
+
+<b>★ /stats</b> - دریافت ۱۰ آهنگ برتر از آمار جهانی، ۱۰ کاربر برتر ربات، ۱۰ چت برتر در ربات، ۱۰ پخش برتر در یک چت و غیره.
+
+<b>★ /sudolist</b> - بررسی کاربران سودو ربات
+
+<b>★ /lyrics [نام آهنگ]</b> - جستجوی متن آهنگ برای موسیقی خاص در وب.
+
+<b>★ /song [نام آهنگ] یا [لینک YT]</b> - دانلود هر آهنگی از یوتیوب در فرمت MP3 یا MP4.
+
+<b>★ /player</b> - دریافت یک پنل تعاملی در حال پخش.
+
+<b>★ /queue یا /cqueue</b> - بررسی لیست صف موسیقی.
+
+    <u><b>⚡️ربات خصوصی:</b></u>
+      
+<b>✧ /authorize [شناسه چت]</b> - اجازه دادن به یک چت برای استفاده از ربات شما.
+
+<b>✧ /unauthorize [شناسه چت]</b> - لغو اجازه یک چت برای استفاده از ربات شما.
+
+<b>✧ /authorized</b> - بررسی تمام چت‌های مجاز برای استفاده از ربات شما.
+"""
+            
