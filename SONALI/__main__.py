ba6c8281f42e4180a34b7e041a08d481d444d539
moharanger <m33537924@gmail.com>
2024-11-01 14:06:20 +0330

    ᴀᴘʟʜᴀ

--- SONALI/__main__.py
+++ SONALI/__main__.py
@@ -37,6 +37,9 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         importlib.import_module("SONALI.plugins" + all_module)
+    if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                HELPABLE[imported_module.__MODULE__.lower()] = imported_module    
     LOGGER("SONALI.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
     await userbot.start()
     await RAUSHAN.start()
